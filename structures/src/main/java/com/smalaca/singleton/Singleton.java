7056a256a1efb39d72e8129bb79b8a8264d538ba
smalaca <ab13.krakow@gmail.com>
2018-10-25 08:59:06 +0200

    Structure - Singleton

---
+++ structures/src/main/java/com/smalaca/singleton/Singleton.java
@@ -0,0 +1,13 @@
+package com.smalaca.singleton;
+
+public class Singleton {
+    private static final Singleton SINGLETON = new Singleton();
+
+    private Singleton() {
+
+    }
+
+    public static Singleton instance() {
+        return SINGLETON;
+    }
+}
