7ec166de4ae075f9236457136e4d44c7709c30f3
smalaca <ab13.krakow@gmail.com>
2019-01-16 11:42:02 +0100

    REFACTORING - introduce duplication in the method

--- examples/src/main/java/com/smalaca/flyweight/translator/TranslatorsFlyweight.java
+++ examples/src/main/java/com/smalaca/flyweight/translator/TranslatorsFlyweight.java
@@ -6,7 +6,6 @@ import com.smalaca.flyweight.external.google.GoogleTranslator;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.function.Supplier;
 
 import static com.smalaca.flyweight.translator.TranslatorType.ENGLISH;
 import static com.smalaca.flyweight.translator.TranslatorType.GERMAN;
@@ -15,18 +14,19 @@ public class TranslatorsFlyweight {
     private final Map<TranslatorType, Translator> translators = new HashMap<>();
 
     public Translator english() {
-        return aTranslator(ENGLISH, () -> new EnglishTranslator(new GoogleTranslator()));
+        if (!translators.containsKey(ENGLISH)) {
+            translators.put(ENGLISH, new EnglishTranslator(new GoogleTranslator()));
+        }
+
+        return translators.get(ENGLISH);
     }
 
     public Translator german() {
-        return aTranslator(GERMAN, () -> new GermanTranslator(new GermanGovWebDictionary()));
-    }
-
-    private Translator aTranslator(TranslatorType translatorType, Supplier<Translator> translator) {
-        if (!translators.containsKey(translatorType)) {
-            translators.put(translatorType, translator.get());
+        if (!translators.containsKey(GERMAN)) {
+            translators.put(GERMAN, new GermanTranslator(new GermanGovWebDictionary()));
         }
 
-        return translators.get(translatorType);
+        return translators.get(GERMAN);
     }
+
 }
