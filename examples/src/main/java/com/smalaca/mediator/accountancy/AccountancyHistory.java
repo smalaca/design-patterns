c725e2715c2881e61c58824f392d428398929bb2
smalaca <ab13.krakow@gmail.com>
2019-01-16 21:33:57 +0100

    REFACTORING - remove useless attribute from AccountancyHistory

--- examples/src/main/java/com/smalaca/mediator/accountancy/AccountancyHistory.java
+++ examples/src/main/java/com/smalaca/mediator/accountancy/AccountancyHistory.java
@@ -1,16 +1,9 @@
 package com.smalaca.mediator.accountancy;
 
-import com.smalaca.mediator.Mediator;
 import com.smalaca.mediator.usermanagement.Account;
 import com.smalaca.mediator.usermanagement.AccountRequest;
 
 public class AccountancyHistory {
-    private final Mediator mediator;
-
-    public AccountancyHistory(Mediator mediator) {
-        this.mediator = mediator;
-    }
-
     public void openHistoryFor(Account account) {
         // some functionality
     }
